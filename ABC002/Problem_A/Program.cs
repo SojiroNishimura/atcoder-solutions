eec404db2f0a1978b43ae9dde0713b64c75a1729
snishimura <sjiro.nishimura@gmail.com>
2018-03-08 00:31:04 +0100

    Add solution : ABC002/Problem_A

--- ABC002/Problem_A/Program.cs
+++ ABC002/Problem_A/Program.cs
@@ -6,7 +6,13 @@ namespace Problem_A
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            System.Console.SetIn(reader);
+
+            string[] input = Console.ReadLine().Split(' ');
+            var a = int.Parse(input[0]);
+            var b = int.Parse(input[1]);
+            Console.WriteLine($"{Math.Max(a, b)}");
         }
     }
 }
