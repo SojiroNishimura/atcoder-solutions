56e6b0c19c63a36dd0ac3a1404543baa97d603f9
snishimura <snishimura@nishimurounoMBP.home>
2018-03-07 20:01:58 +0100

    Initial commit : Add solution for Problem_A

---
+++ ABC001/Problem_A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            System.Console.SetIn(reader);
+
+            var h1 = int.Parse(Console.ReadLine());
+            var h2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{h1 - h2}");
+        }
+    }
+}
