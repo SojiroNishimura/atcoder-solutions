dc9f3bd8d76e57c5dfee455c2aae817fff67eb38
snishimura <snishimura@nishimurounoMBP.home>
2018-03-07 20:55:17 +0100

    Add solution : Problem_B

---
+++ ABC001/Problem_B/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem_B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            System.Console.SetIn(reader);
+
+            var dist = int.Parse(Console.ReadLine());
+            var vv = 0;
+
+            if (dist >= 100 && dist <= 5000)
+            {
+                vv = dist / 100;
+            }
+            else if (dist >= 6000 && dist <= 30000)
+            {
+                vv = (dist / 1000) + 50;
+            }
+            else if (dist >= 35000 && dist <= 70000)
+            {
+                vv = ((dist / 1000) - 30) / 5 + 80;
+            }
+            else if (dist > 70000)
+            {
+                vv = 89;
+            }
+            Console.WriteLine(vv.ToString("D2"));
+        }
+    }
+}
