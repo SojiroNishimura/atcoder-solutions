7d94df2c5ce36f6969a7f9a4ad11f0e5f20857f7
snishimura <sjiro.nishimura@gmail.com>
2018-03-07 21:01:17 +0100

    Add : ABC002

---
+++ ABC002/Problem_A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
