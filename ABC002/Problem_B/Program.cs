565570adb1854cdc8482084984889c4ff0d0bd68
snishimura <sjiro.nishimura@gmail.com>
2018-03-09 19:14:53 +0100

    Add solution : ABC002/Problem_B

---
+++ ABC002/Problem_B/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem_B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            System.Console.SetIn(reader);
+
+            var str = Console.ReadLine();
+            var ans = "";
+            foreach (var c in str)
+            {
+                if (c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o')
+                {
+                    continue;
+                }
+                ans += c;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
