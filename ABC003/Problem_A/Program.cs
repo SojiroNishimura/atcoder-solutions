7cac345035e35ad490f572882299011fad269bcb
snishimura <sjiro.nishimura@gmail.com>
2018-03-10 12:39:24 +0100

    Add solution : ABC003/Problem_A

---
+++ ABC003/Problem_A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            System.Console.SetIn(reader);
+
+            var done = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for (var i = 1; i <= done; i++)
+            {
+                sum += 10000 * i;
+            }
+            var salary = Math.Round(sum / (double)done);
+            Console.WriteLine($"{salary}");
+        }
+    }
+}
