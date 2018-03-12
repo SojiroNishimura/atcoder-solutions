4c04e1d1f0d8d2418bd0bff1ac80a24517ed3c47
snishimura <sjiro.nishimura@gmail.com>
2018-03-12 12:44:53 +0100

    Add solution : ABC007/Problem_A

---
+++ ABC007/Problem_A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            System.Console.SetIn(reader);
+
+            var tree = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{tree - 1}");
+        }
+    }
+}
