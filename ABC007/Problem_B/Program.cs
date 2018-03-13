d40abdd2f23adab1db1da981c27d1741e09291b2
snishimura <sjiro.nishimura@gmail.com>
2018-03-13 19:19:59 +0100

    Add solution : ABC007/Problem_B

---
+++ ABC007/Problem_B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem_B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var str = Console.ReadLine();
+
+            if (str.Length == 1 && str[0] == 'a')
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine("a");
+            }
+        }
+    }
+}
