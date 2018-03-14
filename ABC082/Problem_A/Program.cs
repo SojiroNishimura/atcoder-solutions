0559da4818022d98cd1ec64c4556626d9580b0ec
snishimura <sjiro.nishimura@gmail.com>
2018-03-14 14:34:54 +0100

    Add solution : ABC082/Problem_A

---
+++ ABC082/Problem_A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            int[] inputs = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToArray();
+            Console.WriteLine($"{Math.Ceiling((inputs[0] + inputs[1]) / 2d)}");
+        }
+    }
+}
