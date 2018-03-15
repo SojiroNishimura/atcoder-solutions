b771472cdd2cd5d94dd3d921a656768dd831617a
snishimura <sjiro.nishimura@gmail.com>
2018-03-15 02:48:15 +0100

    Add solution : ABC0082/Problem_B

---
+++ ABC082/Problem_B/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var s = string.Join("", Console.ReadLine().OrderBy(c => c).ToArray());
+            var t = string.Join("", Console.ReadLine().OrderByDescending(c => c).ToArray());
+            if (s.CompareTo(t) < 0) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
