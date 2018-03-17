039c4d99b6161f20fa328323ec65c8e999a04e56
snishimura <sjiro.nishimura@gmail.com>
2018-03-17 15:50:44 +0100

    Add solution : ABC091/Problem_C(still incomplete)

---
+++ ABC091/Problem_C/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_C
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var n = int.Parse(Console.ReadLine());
+            var red = new List<Tuple<int, int>>();
+            var blue = new List<Tuple<int, int>>();
+            for (var i = 0; i < n; i++)
+            {
+                var p = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                red.Add(Tuple.Create(p[0], p[1]));
+            }
+            for (var i = 0; i < n; i++)
+            {
+                var p = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                blue.Add(Tuple.Create(p[0], p[1]));
+            }
+
+            red.Sort();
+            blue.Sort((t1, t2) => t1.Item2 < t2.Item2 ? -1 : 1);
+            var count = 0;
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    //Console.WriteLine($"r:{red[i]} b:{blue[j]}");
+                    if (blue[j] != null && red[i].Item1 < blue[j].Item1 && red[i].Item2 < blue[j].Item2)
+                    {
+                        count++;
+                        blue[j] = null;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"{count}");
+        }
+    }
+}
