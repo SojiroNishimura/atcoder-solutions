5a464de03634e1fdee5024f61deb4d4bb51f636c
snishimura <sjiro.nishimura@gmail.com>
2018-03-17 15:46:41 +0100

    Add solution : ABC091/Problem_A

---
+++ ABC091/Problem_A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var line = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            if (line[0] + line[1] >= line[2]) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
