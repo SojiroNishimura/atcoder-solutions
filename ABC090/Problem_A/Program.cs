66c1ff2e7f96b9791b788606433172de316c2344
snishimura <sjiro.nishimura@gmail.com>
2018-03-18 14:27:49 +0100

    Add solution : ABC090/Problem_A

---
+++ ABC090/Problem_A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            const int N = 3;
+            var str = "";
+            for (var i = 0; i < N; i++)
+            {
+                var row = Console.ReadLine();
+                for (var j = 0; j < N; j++)
+                {
+                    if (i == j) str += row[j];   
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
