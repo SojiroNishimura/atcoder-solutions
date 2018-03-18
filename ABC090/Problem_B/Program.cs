80f423dbf3056b39e7786f0336cb7422857a33a6
snishimura <sjiro.nishimura@gmail.com>
2018-03-19 00:37:03 +0100

    Add solution : ABC090/Problem_B

---
+++ ABC090/Problem_B/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var count = 0;
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            for (var i = input[0]; i <= input[1]; i++)
+            {
+                var numStr = i.ToString();
+                if (numStr.Equals(string.Concat(numStr.Reverse()))) count++;
+            }
+            Console.WriteLine($"{count}");
+        }
+    }
+}
