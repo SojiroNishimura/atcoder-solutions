a2b2b06d001e188d976aeef347d777c80490190e
snishimura <sjiro.nishimura@gmail.com>
2018-03-20 14:14:19 +0100

    Add solution : ABC069/Problem_A

---
+++ ABC069/Problem_A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Console.WriteLine($"{(input[0] - 1) * (input[1] - 1)}");
+        }
+    }
+}
