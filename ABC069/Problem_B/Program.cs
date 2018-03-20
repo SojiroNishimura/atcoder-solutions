7163013519f7ac2e8ae277810b8712401785d75d
snishimura <sjiro.nishimura@gmail.com>
2018-03-20 14:06:50 +0100

    Add solution : ABC069/Problem_B

---
+++ ABC069/Problem_B/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem_B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var str = Console.ReadLine();
+            Console.WriteLine($"{str[0]}{str.Length - 2}{str[str.Length - 1]}");
+        }
+    }
+}
