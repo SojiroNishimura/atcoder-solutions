3ca6c02264c2cd50003e0723f4b3668d602baf08
snishimura <sjiro.nishimura@gmail.com>
2018-03-22 01:23:33 +0100

    Add solution : ABC087/Problem_A

---
+++ ABC087/Problem_A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var x = int.Parse(Console.ReadLine());
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{(x - a) % b}");
+        }
+    }
+}
