ed7acce27bafeb394e397325a1266a0ebe7c709f
snishimura <sjiro.nishimura@gmail.com>
2018-03-22 15:31:01 +0100

    Add solution : ABC081/Problem_A

---
+++ ABC081/Problem_A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var input = Console.ReadLine();
+            var count = 0;
+            foreach (var c in input)
+            {
+                if (c == '1') count++;
+            }
+            Console.WriteLine($"{count}");
+        }
+    }
+}
