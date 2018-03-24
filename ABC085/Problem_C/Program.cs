52b48783459af3199dc47446151a0959a8e3f9c3
snishimura <sjiro.nishimura@gmail.com>
2018-03-24 02:52:38 +0100

    Add solution : ABC085/Problem_C

---
+++ ABC085/Problem_C/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        var reader = new System.IO.StreamReader("Inputs.txt");
+        Console.SetIn(reader);
+
+        var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int n = input[0], y = input[1];
+        int n10000 = -1, n5000 = -1, n1000 = -1;
+        for (var i = 0; i <= n; i++)
+        {
+            for (var j = 0; i + j <= n; j++)
+            {
+                var c = n - (i + j);
+                if ((10000 * i) + (5000 * j) + (1000 * c) == y)
+                {
+                    n10000 = i;
+                    n5000 = j;
+                    n1000 = c;
+                    goto END;
+                }
+            }
+        }
+        END:;
+        Console.WriteLine($"{n10000} {n5000} {n1000}");
+    }
+}
