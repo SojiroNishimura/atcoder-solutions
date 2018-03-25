f51a75d0f26aa68b098705bb0cb49acdf4ec4731
snishimura <sjiro.nishimura@gmail.com>
2018-03-26 01:34:18 +0200

    Add solution : ABC088/Problem_B

---
+++ ABC088/ABC088/Problem_B.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ABC088
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+            Console.WriteLine("Problem_B");
+        }
+
+        public void Solve()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var cards = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(cards, (a, b) => b - a);
+            var alice = 0;
+            var bob = 0;
+            for (var i = 0; i < n; i++)
+            {
+                if ((i + 1) % 2 == 1) alice += cards[i];
+                else bob += cards[i];
+            }
+            Console.WriteLine($"{alice - bob}");
+        }
+    }
+}
---
+++ ABC088/ABC088/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC088
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Problem_B().Solve();
+        }
+    }
+}
