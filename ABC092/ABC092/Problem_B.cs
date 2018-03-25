be4a38080db155bc590b15de936f9ab8f9c5f4ff
snishimura <sjiro.nishimura@gmail.com>
2018-03-25 16:40:19 +0200

    Add solution : ABC092/Problem_B

---
+++ ABC092/ABC092/Problem_B.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC092
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+            Console.WriteLine("Problem_B");
+        }
+
+        public void Solve()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int d = input[0], x = input[1];
+            var eaten = 0;
+            var a = new int[n];
+            for (var i = 0; i < n; i++) a[i] = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < d; i++)
+            {
+                foreach (var num in a)
+                {
+                    if (i * num + 1 <= d) eaten++;
+                }
+                //Console.WriteLine($"eaten:{eaten}");
+            }
+            Console.WriteLine($"{x + eaten}");
+        }
+    }
+}
--- ABC092/ABC092/Program.cs
+++ ABC092/ABC092/Program.cs
@@ -7,7 +7,7 @@ namespace ABC092
         public static void Main(string[] args)
         {
             new Problem_A().Solve();
-            //new Problem_B().Solve();
+            new Problem_B().Solve();
             //new Problem_C().Solve();
         }
     }
