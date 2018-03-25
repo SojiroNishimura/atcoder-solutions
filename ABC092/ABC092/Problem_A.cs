8dbab616935226b3c2c6a9f592abae909105ef69
snishimura <sjiro.nishimura@gmail.com>
2018-03-25 16:36:12 +0200

    Add solution : ABC092/Problem_A

---
+++ ABC092/ABC092/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC092
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            var d = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Math.Min(a, b) + Math.Min(c, d)}");
+        }
+    }
+}
---
+++ ABC092/ABC092/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ABC092
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Problem_A().Solve();
+            //new Problem_B().Solve();
+            //new Problem_C().Solve();
+        }
+    }
+}
