1a8556541a1f91bb97c931b37c4232d15bc0144b
snishimura <sjiro.nishimura@gmail.com>
2018-03-25 01:02:12 +0100

    Add solution : ABC085/Problem_A

---
+++ ABC085/Problem_A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem_A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var s = Console.ReadLine();
+            Console.WriteLine($"{s.Replace("2017", "2018")}");
+        }
+    }
+}
