c8352718ae71fb2f883944ee8c03298b12459069
snishimura <sjiro.nishimura@gmail.com>
2018-03-29 01:12:32 +0200

    Add solution : ABC089/Problem_A

---
+++ ABC089/ABC089/Problem_A.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ABC089
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{n / 3}");
+        }
+    }
+}
