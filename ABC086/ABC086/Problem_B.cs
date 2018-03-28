96d7b0914d12c366b02f22b0d4d42d5e7a6f02a8
snishimura <sjiro.nishimura@gmail.com>
2018-03-28 02:13:03 +0200

    Add solution : ABC086/Problem_B

---
+++ ABC086/ABC086/Problem_B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC086
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var input = Console.ReadLine().Split();
+            var s = string.Join("", input);
+            var root = Math.Floor(Math.Sqrt(int.Parse(s)));
+            if ((root * root).ToString() == s) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
--- ABC086/ABC086/Program.cs
+++ ABC086/ABC086/Program.cs
@@ -7,6 +7,7 @@ namespace ABC086
         public static void Main(string[] args)
         {
             new Problem_A().Solve();
+            new Problem_B().Solve();
         }
     }
 }
