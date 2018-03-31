5fd78538edd3b2073556f3b90af8d5d4b4c2d274
snishimura <sjiro.nishimura@gmail.com>
2018-03-31 15:37:39 +0200

    Add solution : Problem_A

---
+++ ABC084/ABC084/Problem_A.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ABC084
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{48 - n}");            
+        }
+    }
+}
