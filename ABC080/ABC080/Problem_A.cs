fdb650b0a36fd7472a347df472e6640fb1b98511
snishimura <sjiro.nishimura@gmail.com>
2018-04-02 16:51:17 +0200

    Add solution : ABC080/Problem_A

---
+++ ABC080/ABC080/Problem_A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ABC080
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = inputs[0], a = inputs[1], b = inputs[2];
+            Console.WriteLine($"{(n * a < b ? n * a : b)}");
+        }
+    }
+}
