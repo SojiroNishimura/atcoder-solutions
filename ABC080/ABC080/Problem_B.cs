8a3fe680c8e8750eea89c797499226f107e7f9cf
snishimura <sjiro.nishimura@gmail.com>
2018-04-06 02:44:10 +0200

    Add solution : ABC080/Problem_B

---
+++ ABC080/ABC080/Problem_B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC080
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var n = Console.ReadLine();
+            var sum = n.Select(c => (int) Char.GetNumericValue(c)).Sum();
+            if (int.Parse(n) % sum == 0) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
