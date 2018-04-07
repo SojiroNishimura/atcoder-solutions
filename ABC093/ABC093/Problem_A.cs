b9a7564b78ff3cd8e72ac01f39041f9a03681e34
snishimura <sjiro.nishimura@gmail.com>
2018-04-07 17:52:20 +0200

    Add solution : ABC093/Problem_A

---
+++ ABC093/ABC093/Problem_A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC093
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var s = Console.ReadLine();
+            if (s[0] != s[1] && s[0] != s[2] && s[1] != s[2]) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
