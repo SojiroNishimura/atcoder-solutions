0ba38b49c9274cc38c3dd09771a4cc6d81f9ad85
snishimura <sjiro.nishimura@gmail.com>
2018-04-08 12:29:46 +0200

    Add solution : ABC078/Problem_A

---
+++ ABC078/ABC078/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC078
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var inputs = Console.ReadLine().Split().Select(s => s[0]).ToArray();
+            var ans = inputs[0] == inputs[1] ? "=" : inputs[0] < inputs[1] ? "<" : ">";
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ ABC078/ABC078/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC078
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Problem_A().Solve();
+        }
+    }
+}
