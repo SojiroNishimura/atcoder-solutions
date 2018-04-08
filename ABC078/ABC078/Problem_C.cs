541c455ca27a4f78a294aa6457cad554efde3448
snishimura <sjiro.nishimura@gmail.com>
2018-04-08 13:04:15 +0200

    Add solution : ABC078/Problem_C

---
+++ ABC078/ABC078/Problem_C.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC078
+{
+    public class Problem_C
+    {
+        public Problem_C()
+        {
+            var reader = new System.IO.StreamReader("Inputs_C.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_C");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = inputs[0], m = inputs[1];
+            var p = Math.Pow(1 / 2d, m);
+            var t = 100 * (n - m) + 1900 * m;
+            Console.WriteLine($"{t * (1 / p)}");
+        }
+    }
+}
--- ABC078/ABC078/Program.cs
+++ ABC078/ABC078/Program.cs
@@ -7,7 +7,8 @@ namespace ABC078
         public static void Main(string[] args)
         {
             //new Problem_A().Solve();
-            new Problem_B().Solve();
+            //new Problem_B().Solve();
+            new Problem_C().Solve();
         }
     }
 }
