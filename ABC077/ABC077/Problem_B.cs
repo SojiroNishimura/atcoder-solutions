eede92163398df648a58b99871a42aae43cc5018
snishimura <sjiro.nishimura@gmail.com>
2018-04-09 14:13:40 +0200

    Add solution : ABC077/Problem_B

---
+++ ABC077/ABC077/Problem_B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC077
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var n = int.Parse(Console.ReadLine());
+            var max = 0;
+            for (var i = 1; i * i <= n; i++) max = i * i;
+            Console.WriteLine($"{max}");
+        }
+    }
+}
---
+++ ABC077/ABC077/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC077
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Problem_B().Solve();
+        }
+    }
+}
