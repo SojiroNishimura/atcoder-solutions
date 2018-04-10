1b5fb7ae82e8b3ef6024cbcda33918845e54c021
snishimura <sjiro.nishimura@gmail.com>
2018-04-10 02:40:16 +0200

    Add solution : ABC077/Problem_A

---
+++ ABC077/ABC077/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC077
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var line1 = Console.ReadLine();
+            var line2 = Console.ReadLine();
+            if (line1[0] == line2[2] && line1[1] == line2[1] && line1[2] == line2[0]) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
--- ABC077/ABC077/Program.cs
+++ ABC077/ABC077/Program.cs
@@ -6,6 +6,7 @@ namespace ABC077
     {
         public static void Main(string[] args)
         {
+            new Problem_A().Solve();
             new Problem_B().Solve();
         }
     }
