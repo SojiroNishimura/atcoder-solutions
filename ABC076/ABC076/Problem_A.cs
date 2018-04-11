ec90685f5be765c04a6377e10e250273cd1bbbd0
snishimura <sjiro.nishimura@gmail.com>
2018-04-11 16:39:24 +0200

    Add solution : ABC076/Problem_A

---
+++ ABC076/ABC076/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC076
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var r = int.Parse(Console.ReadLine());
+            var g = int.Parse(Console.ReadLine());
+            // (r + x) / 2 = g;
+            Console.WriteLine($"{2 * g - r}");
+        }
+    }
+}
