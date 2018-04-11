729c184fe45d827c07cd335cfe6cde98d2726fbe
snishimura <sjiro.nishimura@gmail.com>
2018-04-12 00:32:16 +0200

    Add solution : ABC076/Problem_B

---
+++ ABC076/ABC076/Problem_B.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ABC076
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+            var min = 1;
+            for (var i = 0; i < n; i++)
+                min = Math.Min(min * 2, min + k);
+            Console.WriteLine($"{min}");
+        }
+    }
+}
