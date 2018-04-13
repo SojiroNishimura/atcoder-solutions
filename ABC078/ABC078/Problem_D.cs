594ed06e4d67870b6052524818b25457ef98b484
snishimura <sjiro.nishimura@gmail.com>
2018-04-13 02:06:27 +0200

    Add solution : ABC078/Problem_D

---
+++ ABC078/ABC078/Problem_D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC078
+{
+    public class Problem_D
+    {
+        public Problem_D()
+        {
+            var reader = new System.IO.StreamReader("Inputs_D.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_D");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var deck = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = inputs[0], z = inputs[1], w = inputs[2];
+            var score = n >= 2 ? Math.Max(Math.Abs(deck[n - 1] - w), Math.Abs(deck[n - 2] - deck[n - 1])) : Math.Abs(deck[0] - w);
+            Console.WriteLine($"{score}");
+        }
+    }
+}
