59d15399fcece0029848702a6147b990fd80e0cb
snishimura <sjiro.nishimura@gmail.com>
2018-04-15 15:43:25 +0200

    Add solution : ABC094/Problem_C

---
+++ ABC094/ABC094/Problem_C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ABC094
+{
+    public class Problem_C
+    {
+        public Problem_C()
+        {
+            var reader = new System.IO.StreamReader("Inputs_C.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_C");
+            var n = int.Parse(Console.ReadLine());
+            var x = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var y = new int[n];
+            Array.Copy(x, y, x.Length);
+            Array.Sort(y);
+
+            foreach (var val in x)
+            {
+                if (val < y[n / 2]) Console.WriteLine($"{y[n / 2]}");
+                else Console.WriteLine($"{y[(n / 2) - 1]}");
+            }
+        }
+    }
+}
