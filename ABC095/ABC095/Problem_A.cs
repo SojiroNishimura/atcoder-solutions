e315053fc9fefd9b9dde72d22696a6dc0bd99782
snishimura <sjiro.nishimura@gmail.com>
2018-04-21 15:49:34 +0200

    Add solution : ABC095/Problem_A

---
+++ ABC095/ABC095/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC095
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var s = Console.ReadLine();
+            var p = 700;
+            foreach (var c in s) if (c == 'o') p += 100;
+            Console.WriteLine($"{p}");
+        }
+    }
+}
