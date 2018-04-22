4738669315d80cc25f61472461fb96181354304a
snishimura <sjiro.nishimura@gmail.com>
2018-04-22 13:06:23 +0200

    Add solution : ABC070/Problem_B

---
+++ ABC070/ABC070/Problem_B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC070
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int A = inputs[0], B = inputs[1], C = inputs[2], D = inputs[3];
+            var start = Math.Max(A, C);
+            var end = Math.Min(B, D);
+            Console.WriteLine($"{Math.Max(0, end - start)}");
+        }
+    }
+}
--- ABC070/ABC070/Program.cs
+++ ABC070/ABC070/Program.cs
@@ -6,7 +6,8 @@ namespace ABC070
     {
         public static void Main(string[] args)
         {
-            new Problem_A().Solve();
+            //new Problem_A().Solve();
+            new Problem_B().Solve();
         }
     }
 }
