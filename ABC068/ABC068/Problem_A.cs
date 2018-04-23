5d25a07834070e59942a22aaf88f69f2fcd8e111
snishimura <sjiro.nishimura@gmail.com>
2018-04-23 15:05:31 +0200

    Add solution : ABC068/Problem_A

---
+++ ABC068/ABC068/Problem_A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ABC068
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var n = Console.ReadLine();
+            Console.WriteLine($"ABC{n}");
+        }
+    }
+}
