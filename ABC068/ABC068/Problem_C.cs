5e33b4501bf4ba3c101977d947e03e5b7b16d2ac
snishimura <sjiro.nishimura@gmail.com>
2018-04-24 17:20:33 +0200

    Add solution : ABC068/Problem_C

---
+++ ABC068/ABC068/Problem_C.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ABC068
+{
+    public class Problem_C
+    {
+        public Problem_C()
+        {
+            var reader = new System.IO.StreamReader("Inputs_C.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_C");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int N = inputs[0], M = inputs[1];
+            var paths1 = new bool[N];
+            var paths2 = new bool[N];
+
+            for (var i = 0; i < M; i++)
+            {
+                var path = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int from = path[0] - 1, to = path[1] - 1;
+                if (from == 0) paths1[to] = true;
+                else if (to == N - 1) paths2[from] = true;
+            }
+
+            var isPossible = false;
+            for (var i = 0; i < N; i++)
+            {
+                if (paths1[i] && paths2[i])
+                {
+                    isPossible = true;
+                    break;
+                }
+            }
+            if (isPossible) Console.WriteLine("POSSIBLE");
+            else Console.WriteLine("IMPOSSIBLE");
+        }
+    }
+}
--- ABC068/ABC068/Program.cs
+++ ABC068/ABC068/Program.cs
@@ -7,7 +7,8 @@ namespace ABC068
         public static void Main(string[] args)
         {
             //new Problem_A().Solve();
-            new Problem_B().Solve();
+            //new Problem_B().Solve();
+            new Problem_C().Solve();
         }
     }
 }
