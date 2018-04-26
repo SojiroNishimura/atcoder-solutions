ae559f80ca21e680d58afd047fd114c5e2e30bb9
snishimura <sjiro.nishimura@gmail.com>
2018-04-26 14:35:56 +0200

    Add solution : ABC067/Problem_A

---
+++ ABC067/ABC067/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC067
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int A = inputs[0], B = inputs[1];
+            if (A % 3 == 0 || B % 3 == 0 || (A + B) % 3 == 0) Console.WriteLine("Possible");
+            else Console.WriteLine("Impossible");
+        }
+    }
+}
