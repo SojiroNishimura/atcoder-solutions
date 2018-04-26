3d914986013e853a806f4def6cb86eed2aa7369f
snishimura <sjiro.nishimura@gmail.com>
2018-04-26 14:45:16 +0200

    Add solution : ABC067/Problem_B

---
+++ ABC067/ABC067/Problem_B.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC067
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int N = inputs[0], K = inputs[1];
+            var lengths = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var ans = lengths.OrderBy(l => l).Reverse().Take(K).ToArray().Sum();
+            Console.WriteLine($"{ans}");
+        }
+    }
+}
