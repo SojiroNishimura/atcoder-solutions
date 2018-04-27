88a0186d5313244ef48743299bac249b96c0e646
snishimura <sjiro.nishimura@gmail.com>
2018-04-27 14:34:41 +0200

    Add solution : ABC074/Problem_A

---
+++ ABC074/ABC074/Problem_A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC074
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var N = int.Parse(Console.ReadLine());
+            var A = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{(N * N) - A}");
+        }
+    }
+}
