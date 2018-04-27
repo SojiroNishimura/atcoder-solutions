c4eadf2a76727f49e1e178f6a368411491b21bc9
snishimura <sjiro.nishimura@gmail.com>
2018-04-27 15:01:40 +0200

    Add solution : ABC074/Problem_B

---
+++ ABC074/ABC074/Problem_B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ABC074
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var N = int.Parse(Console.ReadLine());
+            var K = int.Parse(Console.ReadLine());
+            var x = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var total = 0;
+            for (var i = 0; i < N; i++)
+            {
+                total += Math.Min(2 * x[i], 2 * (K - x[i]));
+            }
+            Console.WriteLine($"{total}");
+        }
+    }
+}
