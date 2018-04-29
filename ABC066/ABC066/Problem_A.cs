c4dc86588c361d7f7d7fc611dfe94236bdcf1dc6
snishimura <sjiro.nishimura@gmail.com>
2018-04-29 15:33:04 +0200

    Add solution : ABC066/Problem_A

---
+++ ABC066/ABC066/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC066
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var prices = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int A = prices[0], B = prices[1], C = prices[2];
+            var min = Math.Min(A + B, Math.Min(A + C, B + C));
+            Console.WriteLine($"{min}");
+        }
+    }
+}
