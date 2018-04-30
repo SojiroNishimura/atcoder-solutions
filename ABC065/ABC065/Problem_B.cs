7e2ca1719b7f977a152034a2d2c960ae27aaf97b
snishimura <sjiro.nishimura@gmail.com>
2018-04-30 15:50:40 +0200

    Add solution : ABC065/Problem_B

---
+++ ABC065/ABC065/Problem_B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ABC065
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var N = int.Parse(Console.ReadLine());
+            var a = new int[N];
+            for (var i = 0; i < N; i++) a[i] = int.Parse(Console.ReadLine());
+
+            var pressed = new bool[N];
+            Console.WriteLine($"{FindAns(0, a, pressed, 0)}");
+        }
+
+        private int FindAns(int next, int[] paths, bool[] visited, int count)
+        {
+            if (visited[next]) return -1;
+            visited[next] = true;
+            if (paths[next] == 2) return count + 1;
+            else return FindAns(paths[next] - 1, paths, visited, ++count);
+        }
+    }
+}
--- ABC065/ABC065/Program.cs
+++ ABC065/ABC065/Program.cs
@@ -6,7 +6,8 @@ namespace ABC065
     {
         public static void Main(string[] args)
         {
-            new Problem_A().Solve();
+            //new Problem_A().Solve();
+            new Problem_B().Solve();
         }
     }
 }
