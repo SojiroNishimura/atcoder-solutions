ab794cd39b5afdc992b822b627358f4001b9ce0c
snishimura <sjiro.nishimura@gmail.com>
2018-05-03 02:35:20 +0200

    Add solution : ABC064/Problem_A

---
+++ ABC064/ABC064/Problem_A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC064
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var s = Console.ReadLine();
+            //var n = s.;
+            if (int.Parse(s.Replace(" ", "")) % 4 == 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+
+        }
+    }
+}
