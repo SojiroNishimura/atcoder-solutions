337728ca9c74e9ca094bc3af2db04b4786a87618
snishimura <sjiro.nishimura@gmail.com>
2018-05-05 17:15:15 +0200

    Add solution : ABC096/Problem_D

---
+++ ABC096/ABC096/Problem_D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC096
+{
+    public class Problem_D
+    {
+        public Problem_D()
+        {
+            var reader = new System.IO.StreamReader("Inputs_D.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_D");
+            var N = int.Parse(Console.ReadLine());
+            var primes = new List<int>();
+
+            for (var i = 2; i < 55555; i++)
+                if (isPrime(i)) primes.Add(i);
+
+            var ans = primes.FindAll(p => p % 5 == 1).Take(N).ToArray();
+            Console.WriteLine($"{string.Join(" ", ans)}");
+        }
+
+        private bool isPrime(int n)
+        {
+            for (var i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
