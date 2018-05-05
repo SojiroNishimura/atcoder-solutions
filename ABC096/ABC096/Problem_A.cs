1ef8c3deb24032901e396f1d71b05d493522d402
snishimura <sjiro.nishimura@gmail.com>
2018-05-05 17:11:13 +0200

    Add solution : ABC096/Problem_A

---
+++ ABC096/ABC096/Problem_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC096
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int m = inputs[0], d = inputs[1];
+            var ans = d >= m ? m : m - 1;
+            Console.WriteLine($"{ans}");
+        }
+    }
+}
---
+++ ABC096/ABC096/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC096
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Problem_A().Solve();
+        }
+    }
+}
