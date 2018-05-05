9143ca60b39c2a969ac15246b812cf7ec6a5b757
snishimura <sjiro.nishimura@gmail.com>
2018-05-05 17:13:39 +0200

    Add solution : ABC096/Problem_B

---
+++ ABC096/ABC096/Problem_B.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC096
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var K = int.Parse(Console.ReadLine());
+            int A = inputs[0], B = inputs[1], C = inputs[2];
+            var max = Math.Max(A, Math.Max(B, C));
+            var n = max;
+            for (var i = 0; i < K; i++) n *= 2;
+            Console.WriteLine($"{inputs.Sum() - max + n}");
+        }
+    }
+}
