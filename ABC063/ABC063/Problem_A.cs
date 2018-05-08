d250c6c2afd673dd794e15dd3f5cba08e38343ef
snishimura <sjiro.nishimura@gmail.com>
2018-05-08 16:26:14 +0200

    Add solution : ABC063/Problem_A

---
+++ ABC063/ABC063/Problem_A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC063
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+			var reader = new System.IO.StreamReader("Inputs_A.txt");
+			Console.SetIn(reader);
+        }
+
+		public void Solve()
+		{
+			Console.WriteLine("Problem_A");
+			var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            if (inputs[0] + inputs[1] < 10) Console.WriteLine($"{inputs[0] + inputs[1]}");
+            else Console.WriteLine("error");
+		}
+    }
+}
