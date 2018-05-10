989908e6b1ff0912d3ff7327c9614007678906f0
snishimura <sjiro.nishimura@gmail.com>
2018-05-10 02:04:54 +0200

    Add solution : ABC063/Problem_B

---
+++ ABC063/ABC063/Problem_B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABC063
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+			var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+		public void Solve()
+		{
+			Console.WriteLine("Problem_B");
+			var s = Console.ReadLine();
+			var ss = new HashSet<char>(s);
+			if (s.Length == ss.Count) Console.WriteLine("yes");
+			else Console.WriteLine("no");
+		}
+    }
+}
