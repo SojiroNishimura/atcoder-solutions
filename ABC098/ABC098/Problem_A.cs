4f454274a6b0b26622f299dcd02458264f0413c2
snishimura <sjiro.nishimura@gmail.com>
2018-06-03 23:56:13 +0900

    Add solution : ABC098/Problem_A

---
+++ ABC098/ABC098/Problem_A.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ABC098
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var A = inputs[0];
+            var B = inputs[1];
+            var plus = A + B;
+            var minus = A - B;
+            var multi = A * B;
+            Console.WriteLine($"{Math.Max(plus, Math.Max(minus, multi))}");
+        }
+    }
+}
---
+++ ABC098/ABC098/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC098
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Problem_A().Solve();
+        }
+    }
+}
