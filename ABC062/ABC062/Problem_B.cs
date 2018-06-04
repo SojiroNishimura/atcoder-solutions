ab537820ac9bb7a611b1bdfeb5f64e9f6dd56bf2
snishimura <sjiro.nishimura@gmail.com>
2018-06-04 23:20:57 +0900

    Add solution : ABC062/Problem_B

---
+++ ABC062/ABC062/Problem_B.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ABC062
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int H = inputs[0], W = inputs[1];
+
+            for (var i = 0; i < W+2; i++) Console.Write("#");
+            Console.Write("\n");
+            for (var j = 0; j < H; j++)
+            {
+                Console.Write("#");
+                Console.Write(Console.ReadLine());
+                Console.Write("#\n");
+            }
+            for (var k = 0; k < W+2; k++) Console.Write("#");
+        }
+    }
+}
