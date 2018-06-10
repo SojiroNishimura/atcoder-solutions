29548440630f3579fb938c26579091a2c4ef987d
snishimura <sjiro.nishimura@gmail.com>
2018-06-11 01:30:35 +0900

    Add solution : ABC099/Problem_A

---
+++ ABC099/ABC099/Problem_A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC099
+{
+    public class Problem_A
+    {
+        public Problem_A()
+        {
+            var reader = new System.IO.StreamReader("Inputs_A.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_A");
+            var N = int.Parse(Console.ReadLine());
+            if (N < 1000) Console.WriteLine("ABC");
+            else Console.WriteLine("ABD");
+        }
+    }
+}
