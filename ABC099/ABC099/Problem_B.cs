7f79ad0fc31414dee190a981f52ef25e752ea9a2
snishimura <sjiro.nishimura@gmail.com>
2018-06-11 01:32:42 +0900

    Add solution : ABC099/Problem_B

---
+++ ABC099/ABC099/Problem_B.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC099
+{
+    public class Problem_B
+    {
+        public Problem_B()
+        {
+            var reader = new System.IO.StreamReader("Inputs_B.txt");
+            Console.SetIn(reader);
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Problem_B");
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int a = inputs[0], b = inputs[1];
+
+            var diff = b - a;
+            var ans = Enumerable.Range(1, diff).Sum() - b;
+            Console.WriteLine(ans);
+        }
+    }
+}
